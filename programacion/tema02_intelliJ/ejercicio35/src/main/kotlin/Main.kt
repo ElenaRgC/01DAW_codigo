7c2d076ea09678d4fccc652b9bd3c13fbf265171
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-13 18:05:06 +0200

    Update Main.kt

--- programacion/tema02_intelliJ/ejercicio35/src/main/kotlin/Main.kt
+++ programacion/tema02_intelliJ/ejercicio35/src/main/kotlin/Main.kt
@@ -4,16 +4,34 @@ import java.util.Scanner
 fun introducirNumeros(cantidadNumeros: Int, valorMaximo: Int) {
 
     var numeroIntroducido: Int = 0
-    val numerosJugador = Array(cantidadNumeros) {0}
+    val numerosJugador = IntArray(cantidadNumeros) {0}
+    var estaRepetido: Boolean
     var i = 0
+    val scanner = Scanner(System.`in`)
 
     while (i < cantidadNumeros) {
         do {
 
             println("Introduzca un número entre 1 y $valorMaximo.")
+            numeroIntroducido = scanner.nextInt()
 
-        } while (estaRepetido && numeroIntroducido >= 1 && numeroIntroducido <= numeroMaximo)
+            estaRepetido = estarRepetido(numerosJugador, numeroIntroducido, i)
 
+            if (estaRepetido) {
+                println("Ha introducido un número que ya había elegido.")
+            }
+
+            if (numeroIntroducido < 1) {
+                println("Ha introducido un número menor que 1.")
+            }
+
+            if (numeroIntroducido > valorMaximo) {
+                println("Ha introducido un número mayor que $valorMaximo.")
+            }
+
+        } while (estaRepetido && numeroIntroducido < 1 && numeroIntroducido > valorMaximo)
+
+    numerosJugador[i] = numeroIntroducido
     i++
     }
 
@@ -34,13 +52,13 @@ fun estarRepetido(arrayNumeros: IntArray, numeroQueComprobar: Int, iteraciones: 
 }
 
 fun main() { // Elena Rodríguez Calderón
-
-    /*
-Genera una combinación al azar para la lotería primitiva.
-Los números de la lotería son 6, comprendidos del 1 al 49.
-En esta versión no importa si se repite algún número.
- */
+    
+    /* Genera una combinación al azar para la lotería primitiva.
+    Los números de la lotería son 6, comprendidos del 1 al 49.
+    En esta versión no importa si se repite algún número.  */
 
     val cantidadNumeros: Int = 6 // Cantidad de números pedidos y generados. En la primitiva son 6.
     val valorMaximo: Int = 49 // Número más alto que se puede seleccionar. En la primitiva es hasta el 49.
+
+
 }
