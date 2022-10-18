fade935e37882e566c55da5b5522c52ab136796e
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-18 14:27:21 +0200

    Updated exercise

---
+++ programacion/tema02_intelliJ/ejercicio18/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() { // Elena Rodríguez Calderón
+
+    /*
+    Dada una hora por teclado (horas, minutos y segundos) realiza un algoritmo que muestre la hora
+    después de incrementarle un segundo.
+     */
+
+
+    println("Introduce la hora.")
+    val horas: Int = readln().toInt()
+
+    println("Introduce los minutos.")
+    val minutos: Int = readln().toInt()
+
+    println("Introduce los segundos.")
+    val segundos: Int = readln().toInt() + 1
+
+ if (segundos - 1 < 60) {
+     println("$horas:$minutos:$segundos")
+ } else {
+     if ()
+ }
+
+}
