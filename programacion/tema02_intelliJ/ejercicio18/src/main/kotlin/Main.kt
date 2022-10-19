30276cd3217790c2a7e3ed7cd2a4132739103ba9
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-19 12:51:53 +0200

    Update Main.kt

--- programacion/tema02_intelliJ/ejercicio18/src/main/kotlin/Main.kt
+++ programacion/tema02_intelliJ/ejercicio18/src/main/kotlin/Main.kt
@@ -7,18 +7,33 @@ fun main() { // Elena Rodríguez Calderón
 
 
     println("Introduce la hora.")
-    val horas: Int = readln().toInt()
+    var horas: Int = readln().toInt()
 
     println("Introduce los minutos.")
-    val minutos: Int = readln().toInt()
+    var minutos: Int = readln().toInt()
 
     println("Introduce los segundos.")
-    val segundos: Int = readln().toInt() + 1
+    var segundos: Int = readln().toInt() + 1
 
  if (segundos - 1 < 60) {
      println("$horas:$minutos:$segundos")
  } else {
-     if ()
+     segundos = segundos - 60
+     minutos = minutos + 1
+
+     if ( minutos + 1 < 60) {
+         println("$horas:$minutos:$segundos")
+     } else {
+         minutos = minutos - 60
+         horas = horas + 1
+
+         if (horas > 23) {
+             horas = 0
+             println("$horas:$minutos:$segundos")
+         } else {
+             println("$horas:$minutos:$segundos")
+         }
+     }
  }
 
 }
