f8024987b8e455a20fe43804ccd19b49bd67da50
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-25 14:02:39 +0200

    Update Main.kt

--- programacion/tema03/strings/ejercicio01/src/main/kotlin/Main.kt
+++ programacion/tema03/strings/ejercicio01/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main() { // Elena Rodríguez Calderón
     // Pedir dos cadenas por teclado y compararlas en: longitud, alfabéticamente y si son iguales.
     println("Introduce una cadena: ")
-    val cadena1 = readln().toString()
+    val cadena1 = readln()
     println("Introduce otra cadena: ")
-    val cadena2 = readln().toString()
+    val cadena2 = readln()
 
     // Comparar longitud
     if (cadena1.length > cadena2.length) {
@@ -15,18 +15,18 @@ fun main() { // Elena Rodríguez Calderón
     }
 
     // Comparar alfabéticamente
-    if (cadena1 > cadena2) {
-        println("$cadena1 estaría después en el diccionario que $cadena2")
-    } else if (cadena1 < cadena2) {
-        println("$cadena1 estaría antes en el diccionario que $cadena2")
+    if (cadena1.lowercase() > cadena2.lowercase()) {
+        println("$cadena1 estaría después en el diccionario que $cadena2.")
+    } else if (cadena1.lowercase() < cadena2.lowercase()) {
+        println("$cadena1 estaría antes en el diccionario que $cadena2.")
     } else {
         println("Las cadenas son la misma.")
     }
 
     // Comparar si son iguales
     if (cadena1 == cadena2) {
-        println("Las cadenas son iguales")
+        println("Las cadenas son iguales.")
     } else {
-        println("Las cadenas no son iguales")
+        println("Las cadenas no son iguales.")
     }
 }
