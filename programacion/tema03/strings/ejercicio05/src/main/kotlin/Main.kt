a8d1bb93b2e059b0c628f704252890dfc4576cf8
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-25 18:01:52 +0200

    Updated Strings Exercise 5

--- programacion/tema03/strings/ejercicio05/src/main/kotlin/Main.kt
+++ programacion/tema03/strings/ejercicio05/src/main/kotlin/Main.kt
@@ -1,13 +1,31 @@
 fun main() { // Elena Rodríguez Calderón
     /*
     Leer una frase y encontrar la palabra de mayor longitud. El programa debe
-imprimir tanto la palabra como el número de caracteres de la misma.
+    imprimir tanto la palabra como el número de caracteres de la misma.
      */
 
     println("Introduce una frase.")
-    val frase = readln()
+    val frase: String = readln()
 
     var palabraMasLarga = ""
+    var palabra = ""
+    var i: Int = 0
+    var j: Int = 0
 
-    while()
+    while (i < frase.length) {
+        if (frase[i] != ' ') { // Si no es un espacio, añadimos letra a letra hasta terminar la palabra en el siguiente espacio.
+            palabra += frase[i]
+            j++
+        } else { // Si es un espacio, estamos en una nueva palabra.
+            if (j > palabraMasLarga.length) { // Comprobamos si la palabra es más larga que la anterior para almacenarla.
+                palabraMasLarga = palabra
+            }
+            palabra = "" // Vaciamos la variable palabra.
+            j = 0 // Reiniciamos el contador de letras.
+        }
+        i++
+    }
+
+    println("La palabra más larga es $palabraMasLarga y tiene $j letras.")
+
 }
