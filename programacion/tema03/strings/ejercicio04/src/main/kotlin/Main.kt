0a5e233085500d283341817c5067176848407d5c
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-10-25 18:26:04 +0200

    Updates on exercises

---
+++ programacion/tema03/strings/ejercicio04/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() { // Elena Rodríguez Calderón
+    /*
+    Dada una cadena, y un carácter, verificar cuántas veces se repite el carácter en la
+    cadena, por ejemplo:
+    Entrada: cad = "casa blanca", car = 'a'
+    Salida: El carácter 'a' se repite 4 veces.
+     */
+
+    // Pedir la cadena y el carácter.
+    println("Introduce una cadena: ")
+    val cadena = readln()
+    println("Introduce otra cadena: ")
+    val caracter = readln()
+
+
+
+}
