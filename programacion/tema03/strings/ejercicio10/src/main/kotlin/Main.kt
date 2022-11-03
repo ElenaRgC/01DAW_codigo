4152d6e0cdc51506ab4bf01e3124203906af5cb8
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-03 10:54:08 +0100

    Update Main.kt

--- programacion/tema03/strings/ejercicio10/src/main/kotlin/Main.kt
+++ programacion/tema03/strings/ejercicio10/src/main/kotlin/Main.kt
@@ -9,4 +9,46 @@ fun main(args: Array<String>) { // Elena Rodríguez Calderón
     c) Remplazar cada dígito encontrado por el siguiente número excepto el 9 que
     deberá ser remplazado por el 0.
      */
+
+    println("Introduce el texto a codificar.")
+    var texto: String = readln().uppercase()
+
+    println("Introduce el número de desplazamientos para codificar.")
+    var numero: Int = readln().toInt()
+
+    var caracterASCII: Int = 0
+    var caracterCodificado: Char
+    var textoCodificado: String = ""
+
+    var i = 0
+
+    while (i < texto.length) {
+        if (texto[i] != ' ') { // No contamos los espacios.
+
+            caracterASCII = texto[i].code
+
+            if (texto[i] == 'Z') {
+                caracterASCII = 64 // Situamos el valor ASCII en el anterior a la A
+            }
+
+            if (texto[i] == '9') {
+                caracterASCII = 47 // Situamos el valor ASCII en el anterior al 0
+            }
+
+            caracterCodificado = (caracterASCII + numero).toChar() // Incluimos el numero de desplazamientos
+
+            textoCodificado += caracterCodificado
+
+        } else {
+            textoCodificado += ' '
+        }
+        i++
     }
+
+    println("El texto original era: ")
+    println(texto)
+    println("El texto codificado con $numero pasos es: ")
+    println(textoCodificado)
+
+
+}
