5e708f5bc512b68951a35014d8e5930bbd382934
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-03 10:26:58 +0100

    New exercises

---
+++ programacion/tema03/strings/ejercicio10/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) { // Elena Rodríguez Calderón
+    /*
+    Un grupo de inteligencia militar desea codificar los mensajes secretos de tal forma que
+    no puedan ser interpretados con una lectura directa, para lo cual han establecido las
+    siguientes reglas (ya inventadas por Julio César):
+    a) Todo mensaje debe tener sus letras en mayúsculas.
+    b) Remplazar cada letra por la que sigue según abecedario, excepto Z que se
+    deberá reemplazar con la letra A.
+    c) Remplazar cada dígito encontrado por el siguiente número excepto el 9 que
+    deberá ser remplazado por el 0.
+     */
+    }
