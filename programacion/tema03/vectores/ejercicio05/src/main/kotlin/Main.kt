263247307be8a0d9c790922d8b45da6c72d3edce
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-09 14:20:55 +0100

    New exercise

--- programacion/tema03/vectores/ejercicio05/src/main/kotlin/Main.kt
+++ programacion/tema03/vectores/ejercicio05/src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
-fun main(args: Array<String>) { // Elena Rodríguez Calderón
+import kotlin.random.Random
 
+fun main(args: Array<String>) { // Elena Rodríguez Calderón
+    /* Diseña un programa que genere un vector con números al azar que oscilan entre
+    [-100 y 100]. Después realiza un módulo que indique cuantos números positivos y
+    cuantos negativos hay. */
+
+    val cantidadNumeros = 5
+
+    var vector = Array(cantidadNumeros){0}
+
+    rellenarVector(vector)
+    positivoNegativo(vector)
+}
+
+fun rellenarVector(vector: Array<Int>) {
+    for (i in vector.indices) {
+        vector[i] = Random.nextInt(-100,100)
+        print("${vector[i]} ")
+    }
+}
+
+fun positivoNegativo(vector: Array<Int>) {
+    var cantidadPositivos = 0
+    var cantidadNegativos = 0
+    for (i in vector.indices) {
+        if (vector[i] >= 0) { // Considero el 0 como positivo
+            cantidadPositivos++
+        } else {
+            cantidadNegativos++
+        }
+    }
+    println("\nHay $cantidadPositivos numeros positivos y $cantidadNegativos numeros negativos.")
 }
