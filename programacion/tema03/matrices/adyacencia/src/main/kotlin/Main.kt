ef6fa1a9c190102f4d2b5a4f4365d0b5bda516ab
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-15 10:29:56 +0100

    Updated exercise

--- programacion/tema03/matrices/adyacencia/src/main/kotlin/Main.kt
+++ programacion/tema03/matrices/adyacencia/src/main/kotlin/Main.kt
@@ -31,18 +31,20 @@ fun main() { // Elena Rodríguez Calderón
     println("Introduzca la columna del elemento.")
     val COLELEMENTO: Int = readln().toInt() - 1
 
-    i = 0
+    println("Buscamos los adyacentes de [${matriz[FILAELEMENTO][COLELEMENTO]}] que está en la posición ($FILAELEMENTO, $COLELEMENTO).")
+    println("Estos son los adyacentes del elemento (*):")
 
-    println("Este es el elemento(*) con sus adyacentes:")
+    i = 0
     while (i < FILAS) {
         j = 0
         while (j < COLUMNAS) {
             if(FILAELEMENTO == i-1 || FILAELEMENTO == i|| FILAELEMENTO == i+1) {
                 if(COLELEMENTO == j-1 || COLELEMENTO == j|| COLELEMENTO == j+1) {
                     if (FILAELEMENTO == i && COLELEMENTO == j) {
-                        print("*")
-                    }
+                        print("(*) ")
+                    } else {
                     print("[${matriz[i][j]}] ")
+                    }
                 }
             }
             j++
