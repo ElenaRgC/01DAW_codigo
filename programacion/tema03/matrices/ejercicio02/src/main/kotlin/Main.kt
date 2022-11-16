96e9f295bfe3975501c7cb6c6faacdbaf292ef0e
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-16 12:17:44 +0100

    New exercise

--- programacion/tema03/matrices/ejercicio02/src/main/kotlin/Main.kt
+++ programacion/tema03/matrices/ejercicio02/src/main/kotlin/Main.kt
@@ -1,4 +1,19 @@
-fun main(args: Array<String>) { // Elena Rodríguez Calderón
+fun main(args: Array<String>) {
+    /*
+    Escribir un programa que lea una matriz entera de tamaño NxM y un valor entero,
+    y encuentre la primera ocurrencia del valor en la matriz.
+     */
+
+    println("Cuantas filas van a tener tus matrices?")
+    val FILAS = readln().toInt()
+
+    println("Cuantas columnas van a tener tus matriz?")
+    val COLUMNAS = readln().toInt()
+
+    var matriz = Array(FILAS) { IntArray(COLUMNAS) { 0 } }
+
+    rellenarMatriz(matriz, FILAS, COLUMNAS)
+    mostrarMatriz(matriz)
 
 
 }
