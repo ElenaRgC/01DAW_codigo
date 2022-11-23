27b0bedf7566bae5f9230b3fbf6b5fdd98fca0d2
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-11-23 20:18:05 +0100

    New exercise

---
+++ programacion/examen2020_1/ejercicio2/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() { // Elena Rodríguez Calderón
+    /*
+    Vamos a simular el juego de piedra, papel o tijera.
+    Dos jugadores podrán elegir entre las tres opciones, recordamos que:
+        El papel gana a la piedra.
+        La piedra gana a la tijera.
+        La tijera gana al papel.
+        Si son iguales no gana nadie.
+    El juego terminará cuando una de los dos jugadores llegue a tres puntos.
+    Para el humano se pedirá que quiere sacar, el ordenador lo realizará aleatoriamente.
+    Al terminar nos dará un resumen de cuantas veces han sacado piedra en total
+    en tdo el juego, además de la puntuación.
+    El programa se debe realizar modularmente, tendrá que tener control de errores.
+     */
+}
