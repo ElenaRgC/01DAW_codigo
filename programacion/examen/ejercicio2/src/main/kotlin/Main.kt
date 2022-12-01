9b6a5c4b29dfc7af9121daa328b484f103dd1fbe
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-12-01 11:05:59 +0100

    New exercise

---
+++ programacion/examen/ejercicio2/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() { // Elena Rodríguez Calderón
+    /*Vamos a realizar el juego del Bingo, aunque algo modificado.
+    Nuestro bombo tendrá solo 20 números y los cartones sólo 5.
+    Bombo: Del que sacaremos 20 bolas (números del 1 al 20) al azar
+    y que me garantizará que nunca se repetirá ningún valor.
+    También deberá contener un módulo que permita inicializarBombo()
+    que “meterá” todas las bolas en el bombo (pondrá todos los números disponibles otra vez).
+    Además de estos métodos implementa los que necesites.
+    Cartón: Nuestro cartón tendrá solo 5 números que el ordenador nos pedirá por teclado.
+    El ordenador jugará con un cartón generado aleatoriamente.
+    Los números que contiene el cartón serán del 1 al 20. En los cartones no se repiten números.
+    En programa principal debe realizar el juego que ganará el que acabe de marcar antes su cartón.
+    Saldrá un número del bombo y el ordenador marcará los dos cartones
+    y nos mostrara la información por pantalla, la bola que ha salido y los dos cartones.
+*/
+}
