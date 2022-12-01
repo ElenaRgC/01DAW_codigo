3f73f0cf9379fbf0dafcc9e4f37213e787f1eaac
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2022-12-01 10:43:34 +0100

    New exercise

---
+++ programacion/examen/ejercicio1/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main()  { //Elena Rodríguez Calderón
+    /*
+    Realizaremos un programa que genere números aleatorios, del 0 al 99 incluidos,
+    al generar el número aleatorio lo introducirá en un vector en su casilla correspondiente
+    (ejemplo número cero va en la posición cero del vector) si no está ocupada ya.
+    Se debe llevar un contador de las veces que se ha intentado rellenar una casilla del vector.
+    El programa terminara cuando este rellenado tod0 el vector con sus números correspondientes,
+    al finalizar, nos mostrara el vector y el número de veces que ha intentado rellenar cada casilla.
+     */
+
+    
+}
