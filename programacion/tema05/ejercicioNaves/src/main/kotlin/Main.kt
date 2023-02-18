2b0d2f123162347a42ffe48873cea6ec8068e435
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-02-18 20:51:34 +0100

    Update Main.kt

--- programacion/tema05/ejercicioNaves/src/main/kotlin/Main.kt
+++ programacion/tema05/ejercicioNaves/src/main/kotlin/Main.kt
@@ -1,3 +1,39 @@
 fun main() { // Elena Rodríguez Calderón
 
 }
+
+/*
+ * Tet:
+ *  - Tiene un almacén de drones, inicialmente 200.
+ *  - Manda drones a la Tierra.
+ *  - Genera órdenes a los cuadrantes
+ *
+ * Tierra:
+ *  - Tiene una parrilla de 9x9 cuadrantes.
+ *
+ * Cuadrante:
+ *  - Tiene entre 1-3 drones.
+ *  - Tiene un mecánico (Jack)
+ *  - Tiene una supervisora (Vika)
+ *
+ * Drones
+ *  - Estado: operativo, inactivo, fallido
+ *  - Nº dron único
+ *
+ * Órdenes:
+ *  - Completado booleano (no se repite dos veces una orden)
+ *  - Fecha
+ *  - Cuadrante destino
+ *  - Pendiente booleano
+ *  - Método mostrarInfo
+ *  - Contabilizar órdenes
+ *
+ * Orden reparación:
+ *  - Nº dron a reparar
+ *  - Estado antes y después de reparar
+ *
+ * Orden reconocimiento:
+ *  - Área a recorrer (num 1-20)
+ *  - Qué se ha encontrado: animales, vegetales, radiación al azar
+ *
+ */
