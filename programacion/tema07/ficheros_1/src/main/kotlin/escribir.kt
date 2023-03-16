ff2fd73c83c65fa78b19b91e11c6d3ddf801a48c
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-03-16 12:40:52 +0100

    Update files example

--- programacion/tema07/ficheros_1/src/main/kotlin/escribir.kt
+++ programacion/tema07/ficheros_1/src/main/kotlin/escribir.kt
@@ -1,16 +1,15 @@
 import java.io.FileWriter
-import java.io.PrintWriter
+import java.io.PrintWriter // Este es el bueno.
 
-
-fun main(args: Array<String>) {
+fun main() {
     try {
         val ruta = "./Ejemplo/datos.txt"
-        val fw = FileWriter(ruta, true)
-        fw.write("soy escrito con el método write")
-        fw.write("\n")
-        val salida = PrintWriter(fw)
-        salida.println("Línea 1 a escribir en el fichero")
-        salida.println("Línea 2 a escribir en el fichero")
+        val fw = FileWriter(ruta, false) // true añade, false sobreescribe
+        //fw.write("Estoy escrito con el método write.")
+        //fw.write("\n")
+        val salida = PrintWriter(fw) // meto un objeto FileWriter
+        salida.println("Línea 1 a escribir en el fichero.")
+        salida.println("Línea 2 a escribir en el fichero.\n")
         salida.flush() //asegurarnos que todos los datos se escriben antes de cerrar el flujo de escritura
         salida.close()
     } catch (e: Exception) {
