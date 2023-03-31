5395bd48efe0aeaac39efe6a57bdecbc9acbff5a
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-03-31 11:42:17 +0200

    New exercises

---
+++ programacion/tema07/examen_transporte/src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main() { // Elena Rodríguez Calderón
+
+}
+
+/*
+- Paquetes:
+    - Destinatario
+    - Peso
+    - esFragil
+    - estaEntregado
+
+    - Urgentes
+        - Días de retraso
+    - Certificados
+        - Fecha de entrega
+        - Quien firma
+
+- Destinatario:
+    - Nombre
+    - Dirección
+    - Teléfono
+
+- Repartidor
+    - ID
+    - Nombre
+    - Fila asignada
+    - Contador de amonestaciones
+    - Furgoneta
+
+- Furgoneta
+    - Matrícula
+    - Marca
+    - Modelo
+    - Max 4 paquetes
+
+- Sede
+    - qenerarInformes()
+    - comprobarPaquetesEnFila()
+    - amonestarRepartidor() si hay más de 20 paquetes sin repartir
+
+-Informe
+    - sector, fila, columna o toda la ciudad
+    - total de paquetes de cada tipo por sector
+    - resumen de cada paquete
+
+
+
+
+ */
