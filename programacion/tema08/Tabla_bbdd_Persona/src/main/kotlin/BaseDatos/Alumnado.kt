1470961e7c4523716b4275a90703dfe83668ac22
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-05-03 13:01:30 +0200

    New DB example

---
+++ programacion/tema08/Tabla_bbdd_Persona/src/main/kotlin/BaseDatos/Alumnado.kt
@@ -0,0 +1,33 @@
+package BaseDatos
+
+import java.time.LocalDate
+
+class Alumnado {
+    var id:Int=0
+    var nombre:String=""
+    var edad:Int = 0
+    var fechaNacimiento= LocalDate.now()
+    var matriculado:Boolean = false
+    var fechaString: String=""
+
+    constructor(i:Int,n:String,e:Int,f:LocalDate,m:Boolean,fs:String){
+        this.id = i
+        this.nombre = n
+        edad = e
+        fechaNacimiento = f
+        matriculado= m
+        fechaString=fs
+    }
+    constructor(i:Int,n:String,e:Int,f:LocalDate,m:Boolean){
+        this.id = i
+        this.nombre = n
+        edad = e
+        fechaNacimiento = f
+        matriculado= m
+    }
+    override fun toString(): String {
+        return "Alumnado(nombre='$nombre', edad=$edad, fechaNacimiento=$fechaNacimiento, matriculado=$matriculado)"
+    }
+
+
+}
